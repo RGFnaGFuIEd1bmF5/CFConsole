7d506699d43ab5ba36e25af522eb2f1eebfb0ecf
buraktutanlar <burak.tutanlar@gmail.com>
2015-10-01 00:21:30 +0300

    Unnecessary comments are deleted.

--- src/main/java/com/craftingserver/cfconsole/App.java
+++ src/main/java/com/craftingserver/cfconsole/App.java
@@ -14,27 +14,10 @@ import java.util.List;
  */
 public class App {
 
-//    public static DockerClient dockerClient;
-//    public static DockerClientConfig dockerClientConfig;
-
     public static void main(final String[] args) throws Exception {
-//        initDockerClient();
         DockerManager.getInstance(); // to init DockerManager singleton
         initServer();
     }
-
-//    private static void initDockerClient() {
-//        dockerClientConfig = DockerClientConfig.createDefaultConfigBuilder()
-//                .withVersion("1.12")
-//                .withUri("http://178.62.65.18:3131")
-//                .withUsername("craftingserver")
-//                .withPassword("asdfghjkl")
-//                .withEmail("burak.tutanlar@gmail.com")
-//                .withServerAddress("https://index.docker.io/v1/")
-//                .withDockerCertPath("/home/user/.docker")
-//                .build();
-//        dockerClient = DockerClientBuilder.getInstance(dockerClientConfig).build();
-//    }
 
     private static void initServer() {
         Undertow server = Undertow.builder().addHttpListener(8081, "localhost")
